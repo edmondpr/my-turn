54bae14d5eb43d42c8ae3ba022c40b9ee2587907
Edmond Pruteanu <edmondpr@gmail.com>
2014-07-25 03:06:48 +0200

    removed populatedb

--- routes/users.js
+++ routes/users.js
@@ -85,34 +85,3 @@ exports.deleteUser = function(req, res) {
         });
     });
 }
-
-/*--------------------------------------------------------------------------------------------------------------------*/
-// Populate database with sample data -- Only used once: the first time the application is started.
-// You'd typically not find this code in a real-life app, since the database would already exist.
-var populateDB = function() {
-
-    var users = [
-    {
-        name: "EDI",
-				balance: "0",
-				dates: "",
-				persons: ""
-    },
-    {
-        name: "FEDE",
-				balance: "0",
-				dates: "",
-				persons: ""
-    },
-    {
-        name: "ROBI",
-				balance: "0",
-				dates: "",
-				persons: ""
-    }];
-
-    database.collection('users', function(err, collection) {
-        collection.insert(users, {safe:true}, function(err, result) {});
-    });
-
-};
